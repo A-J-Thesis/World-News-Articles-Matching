93042ef1f7004395bc432e59177ec4a13df33aee
JasonPap <jason.92@windowslive.com>
2015-07-09 23:36:40 +0300

    add better handling of GeoNames error codes

--- source/geonames.py
+++ source/geonames.py
@@ -13,6 +13,22 @@ import urllib2
 from bs4 import BeautifulSoup
 
 Cache = dict()
+GeonamesErrCodes = dict()
+GeonamesErrCodes["10"]="Authorization Exception"
+GeonamesErrCodes["11"]="record does not exist"
+GeonamesErrCodes["12"]="other error"
+GeonamesErrCodes["13"]="database timeout"
+GeonamesErrCodes["14"]="invalid parameter"
+GeonamesErrCodes["15"]="no result found"
+GeonamesErrCodes["16"]="duplicate exception"
+GeonamesErrCodes["17"]="postal code not found"
+GeonamesErrCodes["18"]="daily limit of credits exceeded"
+GeonamesErrCodes["19"]="hourly limit of credits exceeded "
+GeonamesErrCodes["20"]="weekly limit of credits exceeded"
+GeonamesErrCodes["21"]="invalid input"
+GeonamesErrCodes["22"]="server overloaded exception"
+GeonamesErrCodes["23"]="service not implemented"
+
 def getCountry(query):
     
     if query in Cache:
@@ -23,18 +39,21 @@ def getCountry(query):
         page_source = response.read()
 
         soup = BeautifulSoup(page_source)
+        check = soup.find("status")
+        if check is not None:
+            val = check.get("value")
+            if val != None:
+                if val in GeonamesErrCodes:
+                    RuntimeError("Geonames: " + GeonamesErrCodes[val] + " , query was: ." + query + ".")
+
         countryname = str(soup.find('countryname'))
         
         result = dict()
-        if countryname is None:
-            result["country"] = ""
-        else:
+        if countryname is not None:
             result["country"] = countryname.replace("<countryname>","").replace("</countryname>","")
 
         name = str(soup.find('name'))
-        if name is None:
-            result["place"] = ""
-        else:
+        if name is not None:
             result["place"] = name.replace("<name>","").replace("</name>","")
         
         Cache[query] = result
